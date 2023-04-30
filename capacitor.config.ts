c8bfa4c93beed8000d493dca088998f16c8a2358
VictorCarlipe <victor.cardoso.felipe@gmail.com>
2023-04-30 18:37:00 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'te-ii-2023-1-locacao-app',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
