3377ba3361460c0f392a5ac3a314b133a3ced5a7
Douglas Daniel Casagrande <douglasdcasagrande@hotmail.com>
2023-05-07 23:25:50 -0300

    validators no form de location (não funcionando 100%)

--- src/app/location/components/location-form/location-form.component.ts
+++ src/app/location/components/location-form/location-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { ActivatedRoute, Route, Router } from '@angular/router';
 import { LocationService } from '../../service/location.service';
 
@@ -28,6 +28,7 @@ export class LocationFormComponent  implements OnInit {
     const [url] = this.activatedRoute.snapshot.url;
     this.editMode = url.path === 'edit';
     this.createMode = !this.editMode;
+    this.initializeForm();
 
     this.locationForm = this.formBuilder.group({
       name: 'Teste De Local',
@@ -52,4 +53,28 @@ export class LocationFormComponent  implements OnInit {
     this.router.navigate(['./location'])
   }
 
+  private initializeForm(){
+    this.locationForm = this.formBuilder.group({
+      nome: [
+        'Local para cadastro',
+        [
+          Validators.required,
+          Validators.maxLength(70)
+        ]
+      ],
+      unit: 'I',
+      capacity: [
+        0,
+        [
+          Validators.required
+        ]
+      ],
+      attributes: [
+        'Informações sobre o local...',
+        [
+          Validators.maxLength(250)
+        ]
+      ]
+    })
+  }
 }
